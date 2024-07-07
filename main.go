60112a30d201d6ad032e4454875818d5cca955f3
grodier <grodier3@gmail.com>
2024-07-06 22:05:07 -0400

    Initial setup of requesting a webpage and printing it as a string

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	client := &http.Client{
+		Timeout: 5 * time.Second,
+	}
+
+	resp, err := client.Get("https://georgerodier.com")
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	fmt.Println(resp.Header.Get("Content-Type"))
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(body))
 }
